dc0b980e5f46665f508b0f13f21fe8f8942de252
esther-j <esther.jang0907@gmail.com>
2018-11-18 23:35:30 -0500

    Added Text Logging + Scroll

--- chat_interface.py
+++ chat_interface.py
@@ -2,6 +2,7 @@
 "Updated Animation Starter Code" template from https://www.cs.cmu.edu/~112/notes/notes-animations-part2.html
 Entry Box Tutorial: http://effbot.org/tkinterbook/entry.htm
 Understanding .pack(): http://effbot.org/tkinterbook/pack.htm
+Text Tutorials: https://www.tutorialspoint.com/python/tk_text.htm http://effbot.org/tkinterbook/text.htm
 """
 
 from tkinter import *
@@ -19,15 +20,19 @@ def init(data):
 def mousePressed(event, data):
     # use event.x and event.y
     pass
-
-def keyPressed(event, data, entry):
-    if event.keysym == "Return":
-        data.userEntry = entry.get()
+    
+def keyPressed(event, data, entry, log):
+    entryLog = entry.get()
+    if event.keysym == "Return" and len(entryLog) > 0:
+        data.userEntry = entryLog
         entry.delete(0, END)
         data.chatLog.append(data.userEntry)
+        log.config(state = NORMAL)
+        log.insert(END, "\n" + data.userEntry)
+        log.yview_pickplace("end")
+        log.config(state = DISABLED)
         print(data.chatLog)
     # use event.char and event.keysym
-    pass
 
 def timerFired(data):
     # userInput = entry.get()
@@ -36,7 +41,7 @@ def timerFired(data):
 
 def redrawAll(canvas, data):
     # draw in canvas
-    pass
+    canvas.create_rectangle(0, 0, data.width, data.height, fill = "sky blue", width = 0)
 
 ####################################
 # use the run function as-is
@@ -48,14 +53,14 @@ def run(width=300, height=300):
         canvas.create_rectangle(0, 0, data.width, data.height,
                                 fill='white', width=0)
         redrawAll(canvas, data)
-        canvas.update()    
+        canvas.update()
 
     def mousePressedWrapper(event, canvas, data):
         mousePressed(event, data)
         redrawAllWrapper(canvas, data)
 
-    def keyPressedWrapper(event, canvas, data, entry):
-        keyPressed(event, data, entry)
+    def keyPressedWrapper(event, canvas, data, entry, log):
+        keyPressed(event, data, entry, log)
         redrawAllWrapper(canvas, data)
 
     def timerFiredWrapper(canvas, data):
@@ -72,11 +77,22 @@ def run(width=300, height=300):
     root = Tk()
     
     entry = Entry(root)
-    entryWidth = data.width // 8
+    entryWidth = data.width // 10
     entry.config(width = entryWidth)
     entry.pack(side = BOTTOM)
     
-    root.resizable(width=False, height=False) # prevents resizing window
+    scrollBar = Scrollbar(root)
+    
+    logWidth = data.width // 8
+    logHeight = data.height // 100
+    log = Text(root, width = logWidth, height = logHeight, yscrollcommand = scrollBar.set, state = DISABLED)
+    scrollBar.config(command=log.yview)
+    
+    log.pack(side = BOTTOM)
+   # log.pack(side = LEFT)
+    scrollBar.pack(side = RIGHT, fill = Y)
+    
+    root.resizable(width=True, height=True) # prevents resizing window
     init(data)
     # create the root and the canvas
     canvas = Canvas(root, width=data.width, height=data.height)
@@ -86,7 +102,7 @@ def run(width=300, height=300):
     root.bind("<Button-1>", lambda event:
                             mousePressedWrapper(event, canvas, data))
     root.bind("<Key>", lambda event:
-                            keyPressedWrapper(event, canvas, data, entry))
+                            keyPressedWrapper(event, canvas, data, entry, log))
     timerFiredWrapper(canvas, data)
     # and launch the app
     root.mainloop()  # blocks until window is closed
