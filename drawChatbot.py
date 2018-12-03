0da32ee861dc3530130e6e25b5a09e2ad7864d2f
esther-j <esther.jang0907@gmail.com>
2018-12-03 03:42:36 -0500

    Added different faces, tested blink

--- drawChatbot.py
+++ drawChatbot.py
@@ -1,30 +1,47 @@
 # Draw the bot!
+from tkinter import *
+# # draws the bot's eyes
+# def drawEyes(canvas, data, pixelLen):
+#     halfPixel = pixelLen / 2
+#     canvas.create_rectangle(data.width / 4 - halfPixel, data.height / 3 - halfPixel, data.width / 4 + halfPixel, data.height / 3 + halfPixel, fill = "black")
+#     canvas.create_rectangle(data.width * (3 / 4) - halfPixel, data.height / 3 - halfPixel, data.width * (3 / 4) + halfPixel, data.height / 3 + halfPixel, fill = "black")
+# 
+# # draws the bot's mouth
+# def drawMouth(canvas, data, pixelLen):
+#     canvas.create_rectangle(data.width / 3, data.height / 3 + (pixelLen * 3), data.width * 2 / 3, data.height / 3 + (pixelLen * 4), fill = "black")
+#     canvas.create_rectangle(data.width / 3 - pixelLen, data.height / 3 + (pixelLen * 2), data.width / 3, data.height / 3 + (pixelLen * 3), fill = "black")
+#     canvas.create_rectangle(data.width * 2 / 3, data.height / 3 + (pixelLen * 2), data.width * 2 / 3 + pixelLen, data.height / 3 + (pixelLen * 3), fill = "black")
 
 # draws the bot's eyes
 def drawEyes(canvas, data, pixelLen):
     halfPixel = pixelLen / 2
-    canvas.create_rectangle(data.width / 4 - halfPixel, data.height / 3 - halfPixel, data.width / 4 + halfPixel, data.height / 3 + halfPixel, fill = "black")
-    canvas.create_rectangle(data.width * (3 / 4) - halfPixel, data.height / 3 - halfPixel, data.width * (3 / 4) + halfPixel, data.height / 3 + halfPixel, fill = "black")
+    canvas.create_oval(data.width / 4 - halfPixel, data.height / 3 - halfPixel, data.width / 4 + halfPixel, data.height / 3 + halfPixel, fill = "black")
+    canvas.create_oval(data.width * (3 / 4) - halfPixel, data.height / 3 - halfPixel, data.width * (3 / 4) + halfPixel, data.height / 3 + halfPixel, fill = "black")
 
+def drawHalfEyes(canvas, data, pixelLen):
+    quarterPixel = pixelLen / 4
+    halfPixel = pixelLen / 2
+    canvas.create_oval(data.width / 4 - halfPixel, data.height / 3 - quarterPixel, data.width / 4 + halfPixel, data.height / 3 + quarterPixel, fill = "black")
+    canvas.create_oval(data.width * (3 / 4) - halfPixel, data.height / 3 - quarterPixel, data.width * (3 / 4) + halfPixel, data.height / 3 + quarterPixel, fill = "black")
+
+def drawClosedEyes(canvas, data, pixelLen):
+    eighthPixel = pixelLen / 8
+    halfPixel = pixelLen / 2
+    canvas.create_oval(data.width / 4 - halfPixel, data.height / 3 - eighthPixel, data.width / 4 + halfPixel, data.height / 3 + eighthPixel, fill = "black")
+    canvas.create_oval(data.width * (3 / 4) - halfPixel, data.height / 3 - eighthPixel, data.width * (3 / 4) + halfPixel, data.height / 3 + eighthPixel, fill = "black")
+    
 # draws the bot's mouth
-def drawMouth(canvas, data, pixelLen):
-    canvas.create_rectangle(data.width / 3, data.height / 3 + (pixelLen * 3), data.width * 2 / 3, data.height / 3 + (pixelLen * 4), fill = "black")
-    canvas.create_rectangle(data.width / 3 - pixelLen, data.height / 3 + (pixelLen * 2), data.width / 3, data.height / 3 + (pixelLen * 3), fill = "black")
-    canvas.create_rectangle(data.width * 2 / 3, data.height / 3 + (pixelLen * 2), data.width * 2 / 3 + pixelLen, data.height / 3 + (pixelLen * 3), fill = "black")
+def drawHappyMouth(canvas, data, pixelLen):
+    canvas.create_arc(data.width * 2 / 5, data.height / 2, data.width * 3 / 5, data.height / 3, start = 0, extent = -180, style = ARC, width = 10)
 
-class SettingsIcon(object):
-    def __init__(self, len, height, leftCoordinate):
-        self.color = "gray"
-        self.len = len
-        self.height = height
-        self.coord = leftCoordinate
-        
-    def draw(self, canvas):
-        for i in range(3):
-            canvas.create_rectangle(self.coord, self.coord + 2 * i * self.height, self.coord + self.len, self.coord + self.height + 2 * i * self.height, fill = self.color, width = 0)
-            
-    def isPressed(self, mouseX, mouseY):
-        if mouseX >= self.coord and mouseX <= self.coord + self.len:
-            if mouseY >= self.coord and mouseY <= self.coord + (self.height * 5):
-                return True
-        return False
+def drawSadMouth(canvas, data, pixelLen):
+    canvas.create_arc(data.width * 2 / 5, data.height * 5 / 12, data.width * 3 / 5, data.height * 7 / 12, start = 0, extent = 180, style = ARC, width = 10)
+    
+def drawSurprisedMouth(canvas, data, pixelLen):
+    canvas.create_oval(data.width * 9 / 20, data.height / 3, data.width * 11 / 20, data.height / 2, fill = "black")
+    # canvas.create_rectangle(data.width / 3, data.height / 3 + (pixelLen * 3), data.width * 2 / 3, data.height / 3 + (pixelLen * 4), fill = "black")
+    # canvas.create_rectangle(data.width / 3 - pixelLen, data.height / 3 + (pixelLen * 2), data.width / 3, data.height / 3 + (pixelLen * 3), fill = "black")
+    # canvas.create_rectangle(data.width * 2 / 3, data.height / 3 + (pixelLen * 2), data.width * 2 / 3 + pixelLen, data.height / 3 + (pixelLen * 3), fill = "black")
+    
+
+
